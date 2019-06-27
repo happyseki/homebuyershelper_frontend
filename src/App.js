154917ba846d02f58e367710f2cd14b42ee7ef7e
Flatiron School <flatironschool@Flatirons-MacBook-Air-8.local>
2019-06-27 18:29:36 -0400

    created 2 markers

--- src/App.js
+++ src/App.js
@@ -6,22 +6,27 @@ import './App.css';
 import L from 'leaflet';
 
 
-let leafIcon = L.icon({
+const icon = L.icon({
     iconUrl: require('./pin.png'),
     iconSize: new L.Point(36, 50),
     iconAnchor: new L.Point(18, 50),
     popupAnchor: new L.Point(0, -50)
 });
-// L.marker(new L.LatLng(50.5, 30.51), { icon: leafIcon }).addTo(map);
 
+const position = [
+              [40.75496, -73.7612],
+              [40.77933, -73.81225]
+               ]
+
+const mapConfig = {
+                 center: [40.728291, -73.844612],
+                 zoom: 11
+               };
 class App extends React.Component {
   state={
-    houses: [],
-    location:{
-      lat: 51.505,
-      lng: -0.09 },
-    zoom: 13
+    houses: []
   }
+
   componentDidMount(){
     fetch('http://localhost:3000/api/v1/houses')
     .then(res=>res.json())
@@ -30,34 +35,28 @@ class App extends React.Component {
         houses: houses
       }))
     })
-    navigator.geolocation.getCurrentPosition((position)=>{
-      console.log(position)
-      this.setState({
-        location:{
-          lat:position.coords.latitude,
-          lng:position.coords.longitude
-        }
-      })
-    })
   }
   render(){
     // console.log(this.state.houses)
-   const position = [this.state.location.lat, this.state.location.lng]
     return(
         <React.Fragment>
           <Header />
-          <Map className='map' center={position} zoom={this.state.zoom}>
+          <Map className='map' center={mapConfig.center} zoom={mapConfig.zoom}>
           <TileLayer
            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker
-           position={position}
-           icon={leafIcon}>
-           <Popup>
-             <a href="https://www.google.com" target="_blank" rel='noopener noreferrer'>Click!</a>
-           </Popup>
-         </Marker>
+          {position.map((p,index)=>
+            <Marker
+            key={index}
+             position={p}
+             icon={icon}>
+             <Popup>
+               <a href="https://www.google.com" target="_blank" rel='noopener noreferrer'>Click!</a>
+             </Popup>
+           </Marker>
+          )}
+
          </Map>
           <HouseList houses={this.state.houses}/>
         </React.Fragment>
